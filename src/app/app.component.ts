2cbc9073150092f680a2a80ab72068e9a8e280c5
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-03-26 21:16:42 +0200

    changed implementation of the component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+
+  constructor(private service: ApiService) {
+  }
+
+  ngOnInit() {
+    this.showPeople();
+    this.showPerson(1);
+  }
+
+  showPeople() {
+    this.service.getAllPeople().subscribe(
+      next => console.log(next),
+      error => console.log(error),
+      () => console.log('it is working fine')
+    );
+  }
+
+  showPerson(id: number) {
+    this.service.getPerson(id).subscribe(
+      data => console.log(data),
+      error => console.log(error),
+      () => console.log('it is working fine too')
+    );
+  }
+
 }
