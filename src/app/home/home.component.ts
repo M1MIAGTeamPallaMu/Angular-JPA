04b00b93050b01487d161a57f3240babda725af8
Unknown <hugo.palla@gmail.com>
2018-04-03 21:16:42 +0200

    home component list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HomeService} from './home.service';
+import {Home} from './home.model';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  homes: any;
+
+  constructor(private homeService: HomeService) { }
 
   ngOnInit() {
+    this.homeService.getAllHomes().subscribe(
+      data => this.homes = data,
+      error => console.log(error),
+    );
   }
 
 }
