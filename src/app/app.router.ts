bbd5b0e8f7a5d8621b3955c307f4345073a98709
Unknown <hugo.palla@gmail.com>
2018-04-07 22:45:21 +0200

    Adding routes for the new components

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -2,10 +2,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {PersonComponent} from './person/person.component';
 import {ModuleWithProviders} from '@angular/core';
+import {PersonDetailsComponent} from './person/person-details/person-details.component';
+import {PersonFormComponent} from './person/person-form/person-form.component';
 
 export const router: Routes = [
   { path: 'homes', component: HomeComponent },
-  { path: 'people', component: PersonComponent }
+  { path: 'people', component: PersonComponent },
+  { path: 'people/add', component: PersonFormComponent },
+  { path: 'people/:id', component: PersonDetailsComponent },
 
 ];
 
