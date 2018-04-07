8bedf228caf602b7d1e0c0bd7329210ce1be6e97
Unknown <hugo.palla@gmail.com>
2018-04-07 22:45:57 +0200

    Updating person service to retreive all homes of an user

--- src/app/person/person.service.ts
+++ src/app/person/person.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Person} from './person.model';
+import {Home} from "../home/home.model";
 
 @Injectable()
 export class PersonService {
@@ -16,8 +17,17 @@ export class PersonService {
     return this.http.get<Person>('rest/person/' + id);
   }
 
+  public getPersonHomes(id: number) {
+    return this.http.get<Home[]>('rest/person/' + id + '/homes');
+  }
+
   public createPerson(person) {
-    return this.http.post<Person>('rest/person', person);
+    const _headers = new HttpHeaders();
+    const headers = _headers.append('Content-Type', 'application/json');
+    const options =  {
+      headers: headers
+    };
+    return this.http.post<Person>('rest/person', person, options);
   }
 
 }
